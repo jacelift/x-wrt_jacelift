ff4dcc5d90daab9a7bb37870e994dfb4e890b516
Chen Minqiang <ptpt52@gmail.com>
2018-04-18 00:46:03 +0800

    ramips: mir3g/miwifi-r3 sysupgrade compatable with breed

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -220,10 +220,13 @@ define Device/mir3g
   KERNEL := $(KERNEL_DTB) | uImage lzma
   IMAGE_SIZE := 32768k
   UBINIZE_OPTS := -E 5
-  IMAGES := sysupgrade.tar kernel1.bin rootfs0.bin
+  IMAGES := sysupgrade.tar kernel1.bin rootfs0.bin breed-factory.bin
   IMAGE/kernel1.bin := append-kernel
   IMAGE/rootfs0.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
+  IMAGE/breed-factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | \
+							 append-kernel | pad-to $$(KERNEL_SIZE) | \
+							 append-ubi | check-size $$$$(IMAGE_SIZE)
   DEVICE_TITLE := Xiaomi Mi Router 3G
   SUPPORTED_DEVICES += R3G
   DEVICE_PACKAGES := \
