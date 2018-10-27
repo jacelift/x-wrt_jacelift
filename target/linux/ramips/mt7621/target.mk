x-wrt/x-wrt

move wpad-mini depends to target.mk

target/linux/ramips/mt7621/target.mk
@@ -7,6 +7,8 @@ BOARDNAME:=MT7621 based boards
 FEATURES+=nand ramdisk rtc usb
 CPU_TYPE:=24kc
 
+DEFAULT_PACKAGES += wpad-mini
+
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
 endef

jacelift/x-wrt_jacelift

target/linux/ramips/mt7621/target.mk
@@ -7,6 +7,8 @@ BOARDNAME:=MT7621 based boards
 FEATURES+=nand ramdisk rtc usb minor
 CPU_TYPE:=24kc
 
+DEFAULT_PACKAGES += wpad-basic
+
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
 endef