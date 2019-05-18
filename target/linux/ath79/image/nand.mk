ef5ceff02e4fb1db6d98f55a68cdc1c83f9f757a
Jeff Kletsky <git-commits@allycomm.com>
2019-05-18 15:52:40 -0700

    ath79: Remove legacy GL.iNet GL-AR300M NAND target
    Remove the sole board on the legacy ath79 NAND target
    to clarify patches that will reintroduce support
    on the MTD SPI-NAND framework present in Linux 4.19
    
    Cc: Marty E. Plummer <hanetzer@startmail.com>
    
    Signed-off-by: Jeff Kletsky <git-commits@allycomm.com>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -43,22 +43,6 @@ define Device/aerohive_hiveap-121
 endef
 TARGET_DEVICES += aerohive_hiveap-121
 
-define Device/glinet_gl-ar300m-nand
-  ATH_SOC := qca9531
-  DEVICE_VENDOR := GL.iNet
-  DEVICE_MODEL := GL-AR300M
-  DEVICE_VARIANT := NAND
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport
-  KERNEL_SIZE := 2048k
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  VID_HDR_OFFSET := 512
-  IMAGES += factory.ubi
-  IMAGE/sysupgrade.bin := sysupgrade-tar
-  IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
-endef
-TARGET_DEVICES += glinet_gl-ar300m-nand
-
 define Device/zyxel_nbg6716
   ATH_SOC := qca9558
   DEVICE_VENDOR := ZyXEL
