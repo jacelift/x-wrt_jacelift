84e2ca086e0c91dece69e2a121d7b746806696c1
Jeff Kletsky <git-commits@allycomm.com>
2019-06-01 17:51:17 -0700

    ath79: GL-AR300M: Provide NAND support
    The GL.iNet GL-AR300M has been supported by the ar71xx and ath79
    platforms with access to its 16 MB NOR flash, but not its 128 MB
    SPI NAND flash.
    
    This commit provides support for the NAND through the upstream
    SPI-NAND framework. Devices with both NOR and NAND flash can support
    independent firmware on each, with U-Boot able to boot from either.
    The OEM U-Boot will fall back to the NOR firmware after three
    "unsuccessful" boots.
    
    The family of GL-AR300M devices on the ath79 platform now includes:
    
      * glinet,gl-ar300m-lite	"generic" target, NOR-only board
      * glinet,gl-ar300m-nand	"nand" target
      * glinet,gl-ar300m-nor	"nand" target (NAND-aware)
    
    Existing OpenWrt and GL.iNet firmware can be upgraded from NOR
    versions to glinet,gl-ar300m-nor and from NAND versions to
    glinet,gl-ar300m-nand without --force:
    
      (NOR)  gl-ar300m ==> glinet,gl-ar300m-nor ==> glinet,gl-ar300m-nand
      (NAND) gl-ar300m ==> glinet,gl-ar300m-nand
    
      glinet,gl-ar300m-nor <=> glinet,gl-ar300m-nand
    
    Upgrades are not possible using sysupgrade from legacy NOR to NAND
    
    The GL-AR300M OEM U-Boot allows upload and flashing of either NOR
    firmware (sysupgrade.bin) or NAND firmware (factory.img) through its
    HTTP-based GUI. Serial connectivity is not required.
    
    The boot counter is implemented by the OEM using u-boot-env. At this
    time, it does not appear that the switch on the side of the unit can
    be used to select NOR vs. NAND boot and the fail-over is only from
    NAND to NOR. To save flash wear, it is only reset when running the
    glinet,gl-ar300m-nand firmware.
    
    NAND-specific base-files are used to remove impact on existing
    generic and tiny targets.
    
    As there is now no "generic" build appropriate for the GL-AR300M16,
    (or for users of the GL-AR300M that do not need access to NAND)
    it will be introduced in a subsequent commit.
    
    The ar71xx targets are unmodified.
    
    Signed-off-by: Jeff Kletsky <git-commits@allycomm.com>

--- target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
+++ target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
@@ -9,6 +9,72 @@ platform_check_image() {
 	return 0
 }
 
+RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+nand_nor_do_upgrade() {
+	local upgrade_file="$1"
+
+	local pn
+	local found=""
+	local err
+
+	case "$(get_magic_long "$upgrade_file")" in
+
+	"27051956")	# U-Boot Image Magic
+
+		for pn in "$PART_NAME" "nor_${PART_NAME}" ; do		# firmware
+			if [ "$(find_mtd_index "$pn")" ] ; then
+				PART_NAME="$pn"
+				found="yes"
+				break
+			fi
+		done
+		if [ "$found" = "yes" ] ; then
+			>&2 echo "Running NOR upgrade"
+			default_do_upgrade "$upgrade_file"
+			err=$?
+			if [ "$err" != 0 ] ; then
+				>&2 echo "ERROR: Upgrade failed ($err). Not changing next boot."
+			else
+				type set_next_boot_nor >/dev/null && set_next_boot_nor
+			fi
+		else
+			>&2 echo "ERROR: UPGRADE FAILED: Unable to locate '$PART_NAME' or 'nor_${PART_NAME}'"
+			exit 1
+		fi
+		;;
+
+
+	*)	# otherwise a file that nand_do_upgrade can process
+
+		for pn in "$CI_KERNPART" "nand_${CI_KERNPART}" ; do	# kernel
+			if [ "$(find_mtd_index "$pn")" ] ; then
+				CI_KERNPART="$pn"
+				break
+			fi
+		done
+		for pn in "$CI_UBIPART" "nand_${CI_UBIPART}" ; do	# ubi
+			if [ "$(find_mtd_index "$pn")" ] ; then
+				CI_UBIPART="$pn"
+				break
+			fi
+		done
+		for pn in "$CI_ROOTPART" "nand_${CI_ROOTPART}" ; do	#rootfs
+			if [ "$(find_mtd_index "$pn")" ] ; then
+				CI_ROOTPART="$pn"
+				break
+			fi
+		done
+		>&2 echo "Running NAND upgrade"
+		# TODO: change order when NAND upgrade offers return
+		type set_next_boot_nand >/dev/null && set_next_boot_nand
+		nand_do_upgrade "$upgrade_file"
+		;;
+	esac
+}
+
+
 platform_do_upgrade() {
 	local board=$(board_name)
 
@@ -16,6 +82,10 @@ platform_do_upgrade() {
 	aerohive,hiveap-121|\
 	zyxel,nbg6716)
 		nand_do_upgrade "$1"
+		;;
+	glinet,gl-ar300m-nand|\
+	glinet,gl-ar300m-nor)
+		glinet_nand_nor_do_upgrade "$1"
 		;;
 	*)
 		default_do_upgrade "$1"
