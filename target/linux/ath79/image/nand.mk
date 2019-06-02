14fc992d12d35aba03b7d031f42fc44073c549de
Jeff Kletsky <git-commits@allycomm.com>
2019-06-02 08:18:34 -0700

    ath79: GL-AR750S: Provide NAND Support
    The GL.iNet GL-AR750S has been supported by the ar71xx and ath79
    platforms with access to its 16 MB NOR flash, but not its 128 MB
    SPI NAND flash.
    
    This commit provides support for the NAND through the upstream
    SPI-NAND framework.
    
    At this time, the OEM U-Boot appears to only support loading the
    kernel from NOR. This configuration is preserved as this time,
    with the glinet,gl-ar750s-nand name reserved for a potential,
    future, NAND-only boot.
    
    The family of GL-AR750S devices on the ath79 platform now includes:
    
      * glinet,gl-ar750m-nor-nand	"nand" target
      * glinet,gl-ar750m-nor	"nand" target (NAND-aware)
    
    Existing OpenWrt and GL.iNet firmware can be upgraded from NOR
    versions to glinet,gl-ar750s-nor and from NAND versions to
    glinet,gl-ar750s-nor-nand
    
      (NOR)  gl-ar750s ==> glinet,gl-ar750s-nor ==> glinet,gl-ar750s-nor-nand
      (NAND) gl-ar750s ==> glinet,gl-ar300m-nor-nand
    
      glinet,gl-ar750s-nor <=> glinet,gl-ar750s-nor-nand
    
    Upgrades are not possible using sysupgrade between legacy NOR and NAND
    
    The GL-AR750S OEM U-Boot allows upload and flashing of either NOR
    firmware (sysupgrade.bin) or NAND firmware (factory.img) through its
    HTTP-based GUI. Serial connectivity is not required.
    
    Adds CONFIG_REGULATOR=y and CONFIG_REGULATOR_FIXED_VOLTAGE=y
    to `nand/config-default`to support "turning on" USB-port voltage
    for this device, consistent with previous implementation.
    
    The ar71xx targets are unmodified.
    
    Signed-off-by: Jeff Kletsky <git-commits@allycomm.com>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -106,6 +106,38 @@ define Device/glinet_gl-ar300m-nor
 endef
 TARGET_DEVICES += glinet_gl-ar300m-nor
 
+define Device/glinet_gl-ar750s-common
+  ATH_SOC := qca9563
+  DEVICE_VENDOR := GL.iNet
+  DEVICE_MODEL := GL-AR750S
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9887-ct \
+			kmod-usb2 kmod-usb-storage block-mount
+  KERNEL_SIZE := 2048k
+  IMAGE_SIZE := 16000k
+  PAGESIZE := 2048
+  VID_HDR_OFFSET := 2048
+endef
+
+define Device/glinet_gl-ar750s-nor-nand
+  $(Device/glinet_gl-ar750s-common)
+  DEVICE_VARIANT := NOR/NAND
+  BLOCKSIZE := 128k
+  UBINIZE_OPTS := -E 5
+  IMAGES += factory.img
+  IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar alt-board=gl-ar750s alt-board=glinet_gl-ar750s-nor | append-metadata
+  SUPPORTED_DEVICES += gl-ar750s glinet,gl-ar750s-nor
+endef
+TARGET_DEVICES += glinet_gl-ar750s-nor-nand
+
+define Device/glinet_gl-ar750s-nor
+  $(Device/glinet_gl-ar750s-common)
+  DEVICE_VARIANT := NOR
+  BLOCKSIZE := 64k
+  SUPPORTED_DEVICES += gl-ar750s glinet,gl-ar750s glinet,gl-ar750s-nor-nand
+endef
+TARGET_DEVICES += glinet_gl-ar750s-nor
+
 define Device/zyxel_nbg6716
   ATH_SOC := qca9558
   DEVICE_VENDOR := ZyXEL
