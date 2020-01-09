eccac8f3eb6129e0ffd0ccad568aef286bccaafc
Chen Minqiang <ptpt52@gmail.com>
2020-01-09 20:32:27 +0800

    revert: ramips: disable image build for HiWiFi HC5962

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -370,8 +370,6 @@ define Device/hiwifi_hc5962
   DEVICE_MODEL := HC5962
   DEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 kmod-usb3
   SUPPORTED_DEVICES += hc5962
-  # Kernel partition too small
-  DEFAULT := n
 endef
 TARGET_DEVICES += hiwifi_hc5962
 
