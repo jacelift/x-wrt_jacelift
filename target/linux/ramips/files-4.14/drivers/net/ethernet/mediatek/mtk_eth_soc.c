e9da82c98523504325e8473ac9ee97b854401425
Chen Minqiang <ptpt52@gmail.com>
2020-03-25 14:56:15 +0800

    ramips: net mediatek fix race condition
    Move the setup code to fe_probe(), to resolve the
    race condition issue, which happens when ifconfig
    down/up repeatedly while large traffic passes.
    
    Also refine the fe_poll()
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- target/linux/ramips/files-4.14/drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ target/linux/ramips/files-4.14/drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -842,6 +842,11 @@ static int fe_start_xmit(struct sk_buff *skb, struct net_device *dev)
 	int tx_num;
 	int len = skb->len;
 
+	if (unlikely(test_bit(__FE_DOWN, &priv->state))) {
+		dev_kfree_skb_any(skb);
+		return NETDEV_TX_OK;
+	}
+
 	if (fe_skb_padto(skb, priv)) {
 		netif_warn(priv, tx_err, dev, "tx padding failed!\n");
 		return NETDEV_TX_OK;
@@ -963,16 +968,16 @@ release_desc:
 			rxd->rxd2 = RX_DMA_LSO;
 
 		ring->rx_calc_idx = idx;
+		done++;
+	}
+
+	if (done) {
 		/* make sure that all changes to the dma ring are flushed before
 		 * we continue
 		 */
 		wmb();
 		fe_reg_w32(ring->rx_calc_idx, FE_REG_RX_CALC_IDX0);
-		done++;
 	}
-
-	if (done < budget)
-		fe_reg_w32(rx_intr, FE_REG_FE_INT_STATUS);
 
 	return done;
 }
@@ -1009,14 +1014,7 @@ static int fe_poll_tx(struct fe_priv *priv, int budget, u32 tx_intr,
 	}
 	ring->tx_free_idx = idx;
 
-	if (idx == hwidx) {
-		/* read hw index again make sure no new tx packet */
-		hwidx = fe_reg_r32(FE_REG_TX_DTX_IDX0);
-		if (idx == hwidx)
-			fe_reg_w32(tx_intr, FE_REG_FE_INT_STATUS);
-		else
-			*tx_again = 1;
-	} else {
+	if (idx != hwidx && done) {
 		*tx_again = 1;
 	}
 
@@ -1039,6 +1037,11 @@ static int fe_poll(struct napi_struct *napi, int budget)
 	u32 status, fe_status, status_reg, mask;
 	u32 tx_intr, rx_intr, status_intr;
 
+	if (unlikely(test_bit(__FE_DOWN, &priv->state))) {
+		napi_complete(napi);
+		return 0;
+	}
+
 	status = fe_reg_r32(FE_REG_FE_INT_STATUS);
 	fe_status = status;
 	tx_intr = priv->soc->tx_int;
@@ -1048,6 +1051,10 @@ static int fe_poll(struct napi_struct *napi, int budget)
 	rx_done = 0;
 	tx_again = 0;
 
+	if ((status & (tx_intr | rx_intr))) {
+		fe_reg_w32((status & (tx_intr | rx_intr)), FE_REG_FE_INT_STATUS);
+	}
+
 	if (fe_reg_table[FE_REG_FE_INT_STATUS2]) {
 		fe_status = fe_reg_r32(FE_REG_FE_INT_STATUS2);
 		status_reg = FE_REG_FE_INT_STATUS2;
@@ -1055,11 +1062,15 @@ static int fe_poll(struct napi_struct *napi, int budget)
 		status_reg = FE_REG_FE_INT_STATUS;
 	}
 
-	if (status & tx_intr)
-		tx_done = fe_poll_tx(priv, budget, tx_intr, &tx_again);
+	tx_done = fe_poll_tx(priv, budget, tx_intr, &tx_again);
+	if (tx_done && !tx_again && !(status & tx_intr)) {
+		fe_reg_w32(tx_intr, FE_REG_FE_INT_STATUS);
+	}
 
-	if (status & rx_intr)
-		rx_done = fe_poll_rx(napi, budget, priv, rx_intr);
+	rx_done = fe_poll_rx(napi, budget, priv, rx_intr);
+	if (rx_done && rx_done < budget && !(status & rx_intr)) {
+		fe_reg_w32(rx_intr, FE_REG_FE_INT_STATUS);
+	}
 
 	if (unlikely(fe_status & status_intr)) {
 		if (hwstat && spin_trylock(&hwstat->stats_lock)) {
@@ -1077,20 +1088,13 @@ static int fe_poll(struct napi_struct *napi, int budget)
 	}
 
 	if (!tx_again && (rx_done < budget)) {
-		status = fe_reg_r32(FE_REG_FE_INT_STATUS);
-		if (status & (tx_intr | rx_intr)) {
-			/* let napi poll again */
-			rx_done = budget;
-			goto poll_again;
+		if (likely(napi_complete_done(napi, rx_done) && !test_bit(__FE_DOWN, &priv->state))) {
+			fe_int_enable(tx_intr | rx_intr);
 		}
-
-		napi_complete_done(napi, rx_done);
-		fe_int_enable(tx_intr | rx_intr);
 	} else {
 		rx_done = budget;
 	}
 
-poll_again:
 	return rx_done;
 }
 
@@ -1261,25 +1265,6 @@ static int fe_hw_init(struct net_device *dev)
 static int fe_open(struct net_device *dev)
 {
 	struct fe_priv *priv = netdev_priv(dev);
-	unsigned long flags;
-	u32 val;
-	int err;
-
-	err = fe_init_dma(priv);
-	if (err) {
-		fe_free_dma(priv);
-		return err;
-	}
-
-	spin_lock_irqsave(&priv->page_lock, flags);
-
-	val = FE_TX_WB_DDONE | FE_RX_DMA_EN | FE_TX_DMA_EN;
-	if (priv->flags & FE_FLAG_RX_2B_OFFSET)
-		val |= FE_RX_2B_OFFSET;
-	val |= priv->soc->pdma_glo_cfg;
-	fe_reg_w32(val, FE_REG_PDMA_GLO_CFG);
-
-	spin_unlock_irqrestore(&priv->page_lock, flags);
 
 	if (priv->phy)
 		priv->phy->start(priv);
@@ -1287,12 +1272,11 @@ static int fe_open(struct net_device *dev)
 	if (priv->soc->has_carrier && priv->soc->has_carrier(priv))
 		netif_carrier_on(dev);
 
+	clear_bit(__FE_DOWN, &priv->state);
+
 	napi_enable(&priv->rx_napi);
 	fe_int_enable(priv->soc->tx_int | priv->soc->rx_int);
 	netif_start_queue(dev);
-#ifdef CONFIG_NET_MEDIATEK_OFFLOAD
-	mtk_ppe_probe(priv);
-#endif
 
 	return 0;
 }
@@ -1300,8 +1284,9 @@ static int fe_open(struct net_device *dev)
 static int fe_stop(struct net_device *dev)
 {
 	struct fe_priv *priv = netdev_priv(dev);
-	unsigned long flags;
 	int i;
+
+	set_bit(__FE_DOWN, &priv->state);
 
 	netif_tx_disable(dev);
 	fe_int_disable(priv->soc->tx_int | priv->soc->rx_int);
@@ -1309,13 +1294,6 @@ static int fe_stop(struct net_device *dev)
 
 	if (priv->phy)
 		priv->phy->stop(priv);
-
-	spin_lock_irqsave(&priv->page_lock, flags);
-
-	fe_reg_w32(fe_reg_r32(FE_REG_PDMA_GLO_CFG) &
-		     ~(FE_TX_WB_DDONE | FE_RX_DMA_EN | FE_TX_DMA_EN),
-		     FE_REG_PDMA_GLO_CFG);
-	spin_unlock_irqrestore(&priv->page_lock, flags);
 
 	/* wait dma stop */
 	for (i = 0; i < 10; i++) {
@@ -1326,12 +1304,6 @@ static int fe_stop(struct net_device *dev)
 		}
 		break;
 	}
-
-	fe_free_dma(priv);
-
-#ifdef CONFIG_NET_MEDIATEK_OFFLOAD
-	mtk_ppe_remove(priv);
-#endif
 
 	return 0;
 }
@@ -1576,6 +1548,8 @@ static int fe_probe(struct platform_device *pdev)
 	struct fe_priv *priv;
 	struct clk *sysclk;
 	int err, napi_weight;
+	unsigned long flags;
+	u32 val;
 
 	device_reset(&pdev->dev);
 
@@ -1636,6 +1610,8 @@ static int fe_probe(struct platform_device *pdev)
 		spin_lock_init(&priv->hw_stats->stats_lock);
 	}
 
+	set_bit(__FE_DOWN, &priv->state);
+
 	sysclk = devm_clk_get(&pdev->dev, NULL);
 	if (!IS_ERR(sysclk)) {
 		priv->sysclk = clk_get_rate(sysclk);
@@ -1678,6 +1654,24 @@ static int fe_probe(struct platform_device *pdev)
 		goto err_free_dev;
 	}
 
+	err = fe_init_dma(priv);
+	if (err) {
+		fe_free_dma(priv);
+		goto err_unreg_dev;
+	}
+
+	spin_lock_irqsave(&priv->page_lock, flags);
+	val = FE_TX_WB_DDONE | FE_RX_DMA_EN | FE_TX_DMA_EN;
+	if (priv->flags & FE_FLAG_RX_2B_OFFSET)
+		val |= FE_RX_2B_OFFSET;
+	val |= priv->soc->pdma_glo_cfg;
+	fe_reg_w32(val, FE_REG_PDMA_GLO_CFG);
+	spin_unlock_irqrestore(&priv->page_lock, flags);
+
+#ifdef CONFIG_NET_MEDIATEK_OFFLOAD
+	mtk_ppe_probe(priv);
+#endif
+
 	platform_set_drvdata(pdev, netdev);
 
 	netif_info(priv, probe, netdev, "mediatek frame engine at 0x%08lx, irq %d\n",
@@ -1685,6 +1679,8 @@ static int fe_probe(struct platform_device *pdev)
 
 	return 0;
 
+err_unreg_dev:
+	unregister_netdev(netdev);
 err_free_dev:
 	free_netdev(netdev);
 err_iounmap:
@@ -1697,12 +1693,24 @@ static int fe_remove(struct platform_device *pdev)
 {
 	struct net_device *dev = platform_get_drvdata(pdev);
 	struct fe_priv *priv = netdev_priv(dev);
+	unsigned long flags;
+
+	spin_lock_irqsave(&priv->page_lock, flags);
+	fe_reg_w32(fe_reg_r32(FE_REG_PDMA_GLO_CFG) &
+		     ~(FE_TX_WB_DDONE | FE_RX_DMA_EN | FE_TX_DMA_EN),
+		     FE_REG_PDMA_GLO_CFG);
+	spin_unlock_irqrestore(&priv->page_lock, flags);
+
+#ifdef CONFIG_NET_MEDIATEK_OFFLOAD
+	mtk_ppe_remove(priv);
+#endif
 
 	netif_napi_del(&priv->rx_napi);
 	kfree(priv->hw_stats);
 
 	cancel_work_sync(&priv->pending_work);
 
+	fe_free_dma(priv);
 	unregister_netdev(dev);
 	free_netdev(dev);
 	platform_set_drvdata(pdev, NULL);
