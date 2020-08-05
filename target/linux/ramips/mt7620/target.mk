5abc858a71a2eddb5082a467129340ddc5a18d22
Chen Minqiang <ptpt52@gmail.com>
2020-08-06 07:45:14 +0800

    ramips: switch MT7620 subtarget to 4.14

--- target/linux/ramips/mt7620/target.mk
+++ target/linux/ramips/mt7620/target.mk
@@ -7,6 +7,8 @@ BOARDNAME:=MT7620 based boards
 FEATURES+=usb nand ramdisk
 CPU_TYPE:=24kc
 
+KERNEL_PATCHVER:=4.14
+
 DEFAULT_PACKAGES += kmod-rt2800-soc wpad-basic swconfig
 
 define Target/Description
