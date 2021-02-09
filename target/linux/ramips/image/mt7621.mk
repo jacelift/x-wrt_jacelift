45bb559a876f6fa6135bd949bec967dd8965ea93
Chen Minqiang <ptpt52@gmail.com>
2021-02-09 22:27:32 +0800

    ramips: some devices use ralink-eth driver
    These devices use ralink-eth:
     - xwrt_t-cpe1201k-v01
     - xwrt_t-cpe1200k-v01
     - xwrt_nxc200p
     - xwrt_puppies
     - netgear_r6220
     - netgear_wndr3700-v5
     - d-team_newifi-d2
     - gehua_ghl-r-001
     - xiaomi_redmi-router-ac2100
     - xiaomi_mi-router-ac2100
     - xiaomi_mi-router-3g
     - xiaomi_mi-router-4
     - phicomm_k2p
     - jcg_y2

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -438,6 +438,8 @@ TARGET_DEVICES += dlink_dir-882-r1
 
 define Device/d-team_newifi-d2
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := Newifi
@@ -618,6 +620,8 @@ TARGET_DEVICES += firefly_firewrt
 
 define Device/gehua_ghl-r-001
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 32448k
   DEVICE_VENDOR := GeHua
   DEVICE_MODEL := GHL-R-001
@@ -812,7 +816,6 @@ endef
 TARGET_DEVICES += jcg_q20
 
 define Device/jcg_y2
-  $(Device/dsa-migration)
   $(Device/uimage-lzma-loader)
   IMAGE_SIZE := 16064k
   IMAGES += factory.bin
@@ -1044,6 +1047,8 @@ endef
 
 define Device/netgear_r6220
   $(Device/netgear_sercomm_nand)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   DEVICE_MODEL := R6220
   SERCOMM_HWNAME := R6220
   SERCOMM_HWID := AYA
@@ -1151,6 +1156,8 @@ define Device/netgear_wndr3700-v5
   $(Device/dsa-migration)
   $(Device/netgear_sercomm_nor)
   $(Device/uimage-lzma-loader)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 15232k
   DEVICE_MODEL := WNDR3700
   DEVICE_VARIANT := v5
@@ -1187,6 +1194,8 @@ TARGET_DEVICES += netis_wf2881
 
 define Device/phicomm_k2p
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   IMAGE_SIZE := 15744k
   DEVICE_VENDOR := Phicomm
   DEVICE_MODEL := K2P
@@ -1516,6 +1525,8 @@ TARGET_DEVICES += winstars_ws-wn583a6
 
 define Device/xiaomi_nand_separate
   $(Device/dsa-migration)
+  DEVICE_COMPAT_VERSION := 1.0
+  DEVICE_COMPAT_MESSAGE := Config is compat with swconfig
   $(Device/uimage-lzma-loader)
   DEVICE_VENDOR := Xiaomi
   DEVICE_PACKAGES := uboot-envtools
